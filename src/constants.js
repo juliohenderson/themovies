d31918345906cfada3e8dd2715207628a3a4abaf
Julio Henderson <julio.henderson98@gmail.com>
2019-09-11 10:36:37 -0300

    feat(settings): add constants

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_LANGUAGE = 'language=pt-BR';
+export const API_KEY = 'api_key=3f58cb7b7d4d38dc30f72d0470fba85b';
+export const URL_IMAGE = 'https://image.tmdb.org/t/p';
+export const IMAGE_SIZE_300 = 'w300';
